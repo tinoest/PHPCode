b5f7ecec981ebe6d13cb167391cb4c7c42c40e84
tinoest <tinoest@gmail.com>
2014-03-25 00:04:46 +0000

    Checking in Changes to formatting

--- PHPSerial.php
+++ PHPSerial.php
@@ -1,18 +1,18 @@
 <?php
-				$filename = "/dev/tty.usbserial-A901J3MB";
-        $fd=dio_open( $filename ,O_RDWR | O_NOCTTY | O_NDELAY);
-        dio_fcntl($fd,F_SETFL,0);
-				$result = '';
-				do {
-					$ret =dio_read($fd, 1);
-					if($ret == "\n") {
-						echo "RX: ".$result.PHP_EOL;
-						// Should do something with the data really....
-						
-						$result = '';
-					} 
-					else {
-						$result .= $ret;
-					}
-				} while (true);
+$filename = "/dev/tty.usbserial-A901J3MB";
+$fd=dio_open( $filename ,O_RDWR | O_NOCTTY | O_NDELAY);
+dio_fcntl($fd,F_SETFL,0);
+$result = '';
+do {
+	$ret =dio_read($fd, 1);
+	if($ret == "\n") {
+		echo "RX: ".$result.PHP_EOL;
+		// Should do something with the data really....
+
+		$result = '';
+	} 
+	else {
+		$result .= $ret;
+	}
+} while (true);
 ?>
