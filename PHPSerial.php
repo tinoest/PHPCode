ecde7d8d2cfd1960e9fe8b5fcdf13375c84d9439
tinoest <tinoest@gmail.com>
2014-03-25 19:14:55 +0000

    Checking in changes, added xml to array function

--- PHPSerial.php
+++ PHPSerial.php
@@ -1,4 +1,6 @@
 <?php
+$debug		= true;
+
 $filename = "/dev/tty.usbserial-A901J3MB";
 $fd=dio_open( $filename ,O_RDWR | O_NOCTTY | O_NDELAY);
 dio_fcntl($fd,F_SETFL,0);
@@ -6,13 +8,45 @@ $result = '';
 do {
 	$ret =dio_read($fd, 1);
 	if($ret == "\n") {
-		echo "RX: ".$result.PHP_EOL;
-		// Should do something with the data really....
-
+		if($debug) echo "RX: ".$result.PHP_EOL;
+		$data		= xml2array($result);
+		var_export($data);		
 		$result = '';
 	} 
 	else {
 		$result .= $ret;
 	}
 } while (true);
+
+
+
+
+function xml2array($xml) {{{
+
+    $opened			= array();
+    $xmlParser	= xml_parser_create();
+    xml_parse_into_struct($xmlParser, $xml, $xmlArray);
+    $array			= array();
+    for($j=0;$j<count($xmlArray);$j++){
+        $val		= $xmlArray[$j];
+        switch($val["type"]){
+            case "open":
+                $opened[strtolower($val["tag"])] = $array;
+                unset($array);
+                break;
+            case "complete":
+                $array[strtolower($val["tag"])][] = $val["value"];
+            break;
+            case "close":
+                $opened[strtolower($val["tag"])] = $array;
+                $array = $opened;
+            break;
+        }
+    }
+    return $array;
+
+}}}
+
+
+
 ?>
