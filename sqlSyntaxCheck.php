7ced39fa1bea05d5f465b347223341afd2424f6e
tinoest <tinoest@gmail.com>
2015-07-02 19:42:18 +0100

    Sorted Indent, return checks for positive result consistency

--- sqlSyntaxCheck.php
+++ sqlSyntaxCheck.php
@@ -1,53 +1,54 @@
 <?php
 
-$sql	= "INSERT INTO ( id , value , id ) VALUES ( '{datetime:?1}' , '{string:?2}' , '{ float:?3 }' )";
+$error	= 0;
+$sql		= "INSERT INTO ( id , value , id ) VALUES ( '{datetime:?1}' , '{string:?2}' , '{ float:?3 }' )";
 
-$sql	= config_parse($sql,array('2010-01-10 00:59:49' , 'value' , '1'));
+$sql		= config_parse($sql,array('2010-01-10 00:59:49' , 'value' , '1'));
 
-echo "SQL1 $sql".PHP_EOL;
-$sql = preg_replace_callback('~{( ?[a-z]+):([a-z0-9A-Z: _-]+ ?)}~','sql_match',$sql);
+echo "SQL1 $sql\n";
+$sql = preg_replace_callback('~{( ?[a-z]+):([a-z0-9A-Z: _-]+ ?)}~','sql_syntax_check',$sql);
 
-echo "SQL2 $sql".PHP_EOL;
+if($error > 0)  echo "SQL Parse Error Occurred\n";
+else echo "SQL2 $sql\n";
 
-function sql_match($matches) { 
+function sql_syntax_check($matches) { 
 
-	var_dump($matches);
+	global $error;
 
-	$type	 = trim($matches[1]);
-	$check = trim($matches[2]);
-	switch(trim($type)) {
-		case 'int':
-			if(!is_numeric($check) || (string) $check !== (string) (int)$check) {
-				return FALSE;
-			}
-			return (string)(int)$check;
-			break;
-		case 'float':
-			if (!is_numeric($check)) { 
-				return FALSE;
-			}
-			return (string) (float) $check;
-			break;
-		case 'string':
-			return $check;
-			break;
-		case 'date':
-			if (preg_match('~^([0-9]{4})-([0-1]{2}?)-([0-3]{2}?)$~', $check, $date_matches) === 1) { 
-				return sprintf('%04d-%02d-%02d', $date_matches[1], $date_matches[2], $date_matches[3]);
-			} 
-			return FALSE;
-			break;
-		case 'datetime':
-			if (preg_match('~^([0-9]{4})-([0-1]{2}?)-([0-3]{2}?) ?([0-9]{2}):([0-9]{2}):([0-9]{2})$~', $check, $date_matches) === 1) { 
-				return sprintf('%04d-%02d-%02d %02d:%02d:%02d', $date_matches[1], $date_matches[2], $date_matches[3], $date_matches[4], $date_matches[5], $date_matches[6]);
-			} 
-			return FALSE;
-			break;
+	if(count($matches) === 3) {
 
-		default:	
-			return FALSE;
-		break;
+		$type	 = trim($matches[1]);
+		$check = trim($matches[2]);
+		switch(trim($type)) {
+			case 'int':
+				if(is_numeric($check) || (string) $check === (string) (int)$check) {
+					return sprintf('%i',$check);
+				}
+				break;
+			case 'float':
+				if (is_numeric($check)) { 
+					return sprintf("%f",$check);;
+				}
+				break;
+			case 'string':
+				return $check;
+				break;
+			case 'date':
+				if (preg_match('~^([0-9]{4})-([0-1]{2}?)-([0-3]{2}?)$~', $check, $date_matches) === 1) { 
+					return sprintf('%04d-%02d-%02d', $date_matches[1], $date_matches[2], $date_matches[3]);
+				} 
+				break;
+			case 'datetime':
+				if (preg_match('~^([0-9]{4})-([0-1]{2}?)-([0-3]{2}?) ?([0-9]{2}):([0-9]{2}):([0-9]{2})$~', $check, $date_matches) === 1) { 
+					return sprintf('%04d-%02d-%02d %02d:%02d:%02d', $date_matches[1], $date_matches[2], $date_matches[3], $date_matches[4], $date_matches[5], $date_matches[6]);
+				} 
+				break;
+			default:	
+				break;
+		}
+
 	}
+	$error++;
 
 	return FALSE;
 }
